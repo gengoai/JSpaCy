364627db81888f903f03811f48f9be2505cb0c70
David Bracewell <david@davidbracewell.com>
2022-11-30 16:09:53 -0600

    Formatting

--- src/main/java/com/gengoai/jspacy/JSpacy.java
+++ src/main/java/com/gengoai/jspacy/JSpacy.java
@@ -28,7 +28,7 @@ public class JSpacy {
       PyObject result = (PyObject) python.invoke("pipe", nlp, texts);
       PyIterator docIterator = new PyIterator(result.getAttr("__iter__", PyCallable.class).callAs(PyObject.class));
       List<Doc> documents = new ArrayList<>();
-      while( docIterator.hasNext() ){
+      while (docIterator.hasNext()) {
          documents.add(Doc.from(docIterator.next()));
       }
       return documents;
@@ -36,15 +36,15 @@ public class JSpacy {
 
    public static void main(String[] args) {
       JSpacy spacy = new JSpacy(EN_CORE_WEB_SM);
-      for (Doc doc : spacy.pipe("The man on the hill with the telescope  . He ran a marathon last tuesday.")) {
+      for (Doc doc : spacy.pipe("The man on the hill with the telescope  . He ran a marathon last tuesday.",
+                                "The lady on the mountain with a gun. She murdered Jay with a knife last month.")) {
          for (Span sentence : doc.getSentences()) {
             System.out.println(sentence);
             for (Span entity : sentence.getEntities()) {
-               System.out.println("\t"+ entity.getText() + "/" + entity.getLabel() + " " + entity.getTokens() + "  " + entity.getEntities());
+               System.out.println("\t" + entity.getText() + "/" + entity.getLabel() + " " + entity.getTokens() + "  " + entity.getEntities());
             }
          }
       }
-
    }
 
 
